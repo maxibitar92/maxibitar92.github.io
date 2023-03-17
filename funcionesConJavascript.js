10692d4225e04187eaabe93c8328d1a283d71950
maxibitar92 <103611075+maxibitar92@users.noreply.github.com>
2023-03-16 21:07:18 -0300

    initial commit

---
+++ funcionesConJavascript.js
@@ -0,0 +1,9 @@
+
+
+
+document.getElementById("botonWhatsapp").onclick = function(){
+
+    console.log("capturamos el evento click");
+
+    document.getElementById("mostrarWhatsapp").innerHTML = "+54 351 312-9058 ";
+}
